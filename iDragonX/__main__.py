e6a3a05525ef3e0da3ecf566d153ad5c3f516fa0
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-25 01:19:51 +0530

    Update __main__.py

--- iDragonX/__main__.py
+++ iDragonX/__main__.py
@@ -1,6 +1,10 @@
 from pyrogram import idle, Client, filters
-from iDragonX import app, LOGGER
+from telethon import TelegramClient, events
+from iDragonX import app, teleapp, LOGGER
 from iDragonX.modules import *
 
 print("iDragonX is now ready. Type .alive in any telegram chat.")
 app.run()
+teleapp.start()
+teleapp.run_until_disconnected()
+
