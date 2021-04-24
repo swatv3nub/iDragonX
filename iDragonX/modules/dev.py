53070cfa4bfc65024696c56fa1af84d83b5ea498
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-25 01:14:50 +0530

    Update dev.py

--- iDragonX/modules/dev.py
+++ iDragonX/modules/dev.py
@@ -5,8 +5,7 @@ import os
 import re
 import subprocess
 from io import StringIO
-from iDragonX import app, teleapp, CMD_HELP
-from telethon import events, TelegramClient
+from iDragonX import app, CMD_HELP
 from pyrogram import filters
 
 CMD_HELP.update(
@@ -141,62 +140,3 @@ async def terminal(client, message):
         await message.edit(f"**Output:**\n```{output}```", parse_mode="markdown")
     else:
         await message.edit("**Output:**\n`No Output`")
-
-@teleapp.on(events.NewMessage(outgoing=True, pattern=f"^{PREFIX}evalx (.*)"))
-async def evalx(event):
-    if event.fwd_from:
-        return
-    await event.edit("Running...")
-    cmd = event.text.split(" ", maxsplit=1)[1]
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
-
-    try:
-        await aexecx(cmd, event)
-    except Exception:
-        exc = traceback.format_exc()
-
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success"
-
-    final_output = "Command:\n`{}` \n\nOutput: \n`{}` \n".format(
-        cmd, evaluation
-    )
-
-    if len(final_output) > int("1024"):
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await borg.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id,
-            )
-            await event.delete()
-    else:
-        await event.edit(final_output)
-
-async def aexecx(code, event):
-    exec(f"async def __aexec(event): " + "".join(f"\n {l}" for l in code.split("\n")))
-    return await locals()["__aexec"](event)
